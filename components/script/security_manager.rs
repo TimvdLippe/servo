6db7074a0e08efe2841ce2f5799142757aeaa002
Tim van der Lippe <tvanderlippe@gmail.com>
2025-04-05 12:58:22 +0200

    Update FetchTaskTarget to propagate CSP violations.
    It also updates the FetchResponseListener to process CSP violations
    to ensure that iframe elements (amongst others) properly generate
    the CSP events. These iframe elements are used in the Trusted Types
    tests themselves and weren't propagating the violations before.
    
    However, the tests themselves are still not passing since they also
    use Websockets, which currently aren't using the fetch machinery
    itself. That is fixed as part of [1].
    
    [1]: https://github.com/servo/servo/issues/35028
    
    Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>

--- components/script/security_manager.rs
+++ components/script/security_manager.rs
@@ -26,6 +26,7 @@ use crate::task::TaskOnce;
 pub(crate) struct CSPViolationReporter {
     sample: Option<String>,
     filename: String,
+    directive: String,
     report_only: bool,
     runtime_code: RuntimeCode,
     line_number: u32,
@@ -60,12 +61,14 @@ impl CSPViolationReporter {
         report_only: bool,
         runtime_code: RuntimeCode,
         filename: String,
+        directive: String,
         line_number: u32,
         column_number: u32,
     ) -> CSPViolationReporter {
         CSPViolationReporter {
             sample,
             filename,
+            directive,
             report_only,
             runtime_code,
             line_number,
@@ -95,8 +98,8 @@ impl CSPViolationReporter {
             status_code: global.status_code().unwrap_or(200),
             document_url: self.strip_url_for_reports(global.get_url()),
             source_file: self.filename.clone(),
-            violated_directive: "script-src".to_owned(),
-            effective_directive: "script-src".to_owned(),
+            violated_directive: self.directive.clone(),
+            effective_directive: self.directive.clone(),
             line_number: self.line_number,
             column_number: self.column_number,
             original_policy: String::default(),
